15818b4afef480cc1f478794d3c87cf09e863b24
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2026-01-03 15:52:57 +0300

    unnecessary import removed.

--- financial-helper/src/components/PersonDock.tsx
+++ financial-helper/src/components/PersonDock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './PersonDock.css';
 import type { Person } from '../models';
 import { PersonService } from '../personService';
