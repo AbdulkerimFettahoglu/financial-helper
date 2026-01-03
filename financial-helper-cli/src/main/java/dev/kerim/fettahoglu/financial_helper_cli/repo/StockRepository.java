a6c0c5a3a755a9743d739fc69e21cfe7b17b0968
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2026-01-03 19:45:05 +0300

    typo fixed.

--- financial-helper-cli/src/main/java/dev/kerim/fettahoglu/financial_helper_cli/repo/StockRepository.java
+++ financial-helper-cli/src/main/java/dev/kerim/fettahoglu/financial_helper_cli/repo/StockRepository.java
@@ -3,6 +3,6 @@ package dev.kerim.fettahoglu.financial_helper_cli.repo;
 import dev.kerim.fettahoglu.financial_helper_cli.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockRepository extends JpaRepository<Stock, Integer> {
+public interface StockRepository extends JpaRepository<Stock, Long> {
 
 }
--- financial-helper-cli/src/main/java/dev/kerim/fettahoglu/financial_helper_cli/repo/TransactionRepository.java
+++ financial-helper-cli/src/main/java/dev/kerim/fettahoglu/financial_helper_cli/repo/TransactionRepository.java
@@ -3,6 +3,6 @@ package dev.kerim.fettahoglu.financial_helper_cli.repo;
 import dev.kerim.fettahoglu.financial_helper_cli.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 }
