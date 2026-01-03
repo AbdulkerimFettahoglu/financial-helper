bd3c81bd1f0561b9729e714eae6f042ce86aa557
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2026-01-03 13:38:59 +0300

    bootsrap added to project

--- financial-helper/src/main.tsx
+++ financial-helper/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Bootstrap CSS (önce yükle: npm install bootstrap)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
